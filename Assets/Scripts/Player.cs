d0db5ff7cb3e6545f8387aabcb5587f143f3811e
Angelo :3 <angelobarchada@gmail.com>
2025-05-08 17:04:26 -0300

    Networking base (lo q se pudo)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
     //stats
     [SerializeField] public float HP;
@@ -31,7 +32,7 @@ public class Player : MonoBehaviour
         Jump();
     }
 
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
         Movement(HorizontalInput);
     }
